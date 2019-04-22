9ee41af3540f70a47042041af875c53ba2f2d701
Pavel Buchinchik <bychinpavel@gmail.com>
2019-04-23 00:48:30 +0300

    web: new classes: Sphere, Camera, Graphic

--- sphere.js
+++ sphere.js
@@ -1,255 +1,296 @@
-// Vertex shader program
-var VSHADER_SOURCE =
-  'attribute vec4 a_Position;\n' +
-   //  'attribute vec4 a_Color;\n' + // Defined constant in main()
-  'attribute vec4 a_Normal;\n' +
-  'uniform mat4 u_MvpMatrix;\n' +
-  'uniform mat4 u_ModelMatrix;\n' +    // Model matrix
-  'uniform mat4 u_NormalMatrix;\n' +   // Transformation matrix of the normal
-  'varying vec4 v_Color;\n' +
-  'varying vec3 v_Normal;\n' +
-  'varying vec3 v_Position;\n' +
-  'void main() {\n' +
-  '  vec4 color = vec4(0.8, 0.2, 0.2, 1);\n' + // Sphere color
-  '  gl_Position = u_MvpMatrix * a_Position;\n' +
-     // Calculate the vertex position in the world coordinate
-  '  v_Position = vec3(u_ModelMatrix * a_Position);\n' +
-  '  v_Normal = normalize(vec3(u_NormalMatrix * a_Normal));\n' +
-  '  v_Color = color;\n' + 
-  '}\n';
+const VSHADER_SOURCE = `
+  attribute vec4 a_Position;
+  // 'attribute vec4 a_Color;
+  attribute vec4 a_Normal;
+  uniform mat4 u_MvpMatrix;
+  uniform mat4 u_ModelMatrix; // Model matrix
+  uniform mat4 u_NormalMatrix; // Transformation matrix of the normal
+  varying vec4 v_Color;
+  varying vec3 v_Normal;
+  varying vec3 v_Position;
+  void main() {
+    vec4 color = vec4(0.8, 0.2, 0.2, 1); // Sphere color
+    gl_Position = u_MvpMatrix * a_Position;
+    // Calculate the vertex position in the world coordinate
+    v_Position = vec3(u_ModelMatrix * a_Position);
+    v_Normal = normalize(vec3(u_NormalMatrix * a_Normal));
+    v_Color = color;
+  }`;
 
-// Fragment shader program
-var FSHADER_SOURCE =
-  '#ifdef GL_ES\n' +
-  'precision mediump float;\n' +
-  '#endif\n' +
-  'uniform vec3 u_LightColor;\n' +     // Light color
-  'uniform vec3 u_LightPosition;\n' +  // Position of the light source
-  'uniform vec3 u_AmbientLight;\n' +   // Ambient light color
-  'varying vec3 v_Normal;\n' +
-  'varying vec3 v_Position;\n' +
-  'varying vec4 v_Color;\n' +
-  'void main() {\n' +
-     // Normalize the normal because it is interpolated and not 1.0 in length any more
-  '  vec3 normal = normalize(v_Normal);\n' +
-     // Calculate the light direction and make it 1.0 in length
-  '  vec3 lightDirection = normalize(u_LightPosition - v_Position);\n' +
-     // The dot product of the light direction and the normal
-  '  float nDotL = max(dot(lightDirection, normal), 0.0);\n' +
-     // Calculate the final color from diffuse reflection and ambient reflection
-  '  vec3 diffuse = u_LightColor * v_Color.rgb * nDotL;\n' +
-  '  vec3 ambient = u_AmbientLight * v_Color.rgb;\n' +
-  '  gl_FragColor = vec4(diffuse + ambient, v_Color.a);\n' +
-  '}\n';
+const FSHADER_SOURCE = `
+  #ifdef GL_ES
+  precision mediump float;
+  #endif
+  uniform vec3 u_LightColor; // Light color
+  uniform vec3 u_LightPosition; // Position of the light source
+  uniform vec3 u_AmbientLight; // Ambient light color
+  varying vec3 v_Normal;
+  varying vec3 v_Position;
+  varying vec4 v_Color;
+  void main() {
+    // Normalize the normal because it is interpolated and not 1.0 in length any more
+    vec3 normal = normalize(v_Normal);
+    // Calculate the light direction and make it 1.0 in length
+    vec3 lightDirection = normalize(u_LightPosition - v_Position);
+    // The dot product of the light direction and the normal
+    float nDotL = max(dot(lightDirection, normal), 0.0);
+    // Calculate the final color from diffuse reflection and ambient reflection
+    vec3 diffuse = u_LightColor * v_Color.rgb * nDotL;
+    vec3 ambient = u_AmbientLight * v_Color.rgb;
+    gl_FragColor = vec4(diffuse + ambient, v_Color.a);
+  }`;
+
+
+const SPIRAL_SPHERE = 'spiral';
+const CLASSIC_SPHERE = 'classic';
+
+const DEFAULT_SPHERE_ANGLE_DEG = 0;
+
+const DEFAULT_CAMERA_POS = {x:3, y:3, z:2}
+const DEFAULT_CAMERA_LOOK_AT = {x:0, y:0, z:0}
+const DEFAULT_CAMERA_UP = {x:0, y:0, z:1}
+
+
+class Sphere {
+  // TODO others angles of sphere
+  // TODO handler on mouse drag
+
+  constructor(type, div, radius, center) {
+    this.DIV = div;
+    this.R = radius;
+    this.center = center;
+    this.angleZ = DEFAULT_SPHERE_ANGLE_DEG;
+  
+    if (type === SPIRAL_SPHERE) {
+      this.type = SPIRAL_SPHERE;
+      this.generateSpiralSphere();
+
+    } else if (type === CLASSIC_SPHERE) {
+      this.type = CLASSIC_SPHERE;
+      this.generateClassicSphere();
+    }
+
+    return
+  }
+
+  generateClassicSphere() {
+    this.positions = new Array(3 * (this.DIV + 1) * (this.DIV + 1));
+    this.indices = new Array(4 * this.DIV * this.DIV);
+    let iterator = 0;
+  
+    // Generate coordinates
+    for (let j = 0; j <= this.DIV; ++j) {
+      let phi = j * Math.PI / this.DIV;
+      let sj = Math.sin(phi);
+      let cj = Math.cos(phi);
+  
+      for (let i = 0; i <= this.DIV; ++i) {
+        let theta = 2 * i * Math.PI / this.DIV;
+        let si = Math.sin(theta);
+        let ci = Math.cos(theta);
+        
+        this.positions[iterator++] = this.center.x + (this.R * ci * sj); // X
+        this.positions[iterator++] = this.center.y + (this.R * si * sj); // Y
+        this.positions[iterator++] = this.center.z + (this.R * cj);      // Z
+      }
+    }
+  
+    iterator = 0;
+  
+    // Generate indices (for *_STRIP methods)
+    for (let j = 0; j < this.DIV; ++j) {
+      for (let i = 0; i < this.DIV; ++i) {
+        let p1 = j * (this.DIV + 1) + i;
+        let p2 = p1 + (this.DIV + 1);
+  
+        this.indices[iterator++] = p1;
+        this.indices[iterator++] = p1 + 1;
+        this.indices[iterator++] = p2;
+        this.indices[iterator++] = p2 + 1;
+      }
+    }
+    return;
+  }
+  
+  generateSpiralSphere() {
+    const N = this.DIV;
+  
+    this.positions = new Array(3 * N);
+    let iterator = 0;
+  
+    // Generate coordinates
+    let theta = 0;
+  
+    for (let k = 1; k <= N; ++k) {
+      let h = -1 + (2 * k - 2) / (N - 1);
+      let phi = Math.acos(h);
+  
+      let s_phi = Math.sin(phi);
+      let c_phi = Math.cos(phi);
+  
+      theta = (theta + 3.8 / Math.sqrt(N * (1 - h * h)))// % (2 * Math.PI);
+      if (k == N || k == 1)
+        theta = 0;
+  
+      let s_theta = Math.sin(theta);
+      let c_theta = Math.cos(theta);
+        
+      this.positions[iterator++] = this.center.x + (this.R * s_phi * c_theta); // X
+      this.positions[iterator++] = this.center.y + (this.R * s_phi * s_theta); // Y
+      this.positions[iterator++] = this.center.z + (this.R * c_phi);           // Z
+    }
+  
+    // Generate indices (for POINTS method)
+    this.indices = new Array(N);
+    for (let i = 0; i < N; i++)
+      this.indices[i] = i;
+
+    return;
+  }
+}
+
+
+class Camera {
+  // TODO handler on keys to change camera pos (wasd, qe)
+  constructor() {
+    this.pos = DEFAULT_CAMERA_POS;
+    this.lookAt = DEFAULT_CAMERA_LOOK_AT;
+    this.up = DEFAULT_CAMERA_UP;
+  }
+}
+
+
+class Graphic {
+
+  constructor(sphere) { // TODO sphere to interface
+    this.sphere = sphere;
+    this.camera = new Camera();
+
+    this.canvas = document.getElementById('webgl');
+    this.gl = getWebGLContext(this.canvas);
+    if (!this.gl) {
+      console.error('Failed to get the rendering context for WebGL');
+      return;
+    }
+
+    if (!initShaders(this.gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+      console.error('Failed to intialize shaders.');
+      return;
+    }
+
+    if (!this.initVertexBuffersForSphere()) {
+      console.error('Failed to set the vertex information');
+      return;
+    }
+    this.vertex_n = this.sphere.indices.length
+
+    this.gl.clearColor(1, 1, 1, 1)
+    this.gl.enable(this.gl.DEPTH_TEST);
+
+    this.u_ModelMatrix = this.gl.getUniformLocation(this.gl.program, 'u_ModelMatrix');
+    this.u_MvpMatrix = this.gl.getUniformLocation(this.gl.program, 'u_MvpMatrix');
+    this.u_NormalMatrix = this.gl.getUniformLocation(this.gl.program, 'u_NormalMatrix');
+
+    this.u_LightColor = this.gl.getUniformLocation(this.gl.program, 'u_LightColor');
+    this.u_LightPosition = this.gl.getUniformLocation(this.gl.program, 'u_LightPosition');
+    this.u_AmbientLight = this.gl.getUniformLocation(this.gl.program, 'u_AmbientLight');
+
+    if (!this.u_ModelMatrix || !this.u_NormalMatrix || !this.u_MvpMatrix || !this.u_LightColor || !this.u_LightPosition　|| !this.u_AmbientLight) { 
+      console.error('Failed to get the storage location');
+      return;
+    }
+
+    this.gl.uniform3f(this.u_LightColor, 0.8, 0.8, 0.8); // Set the light color (white)
+    this.gl.uniform3f(this.u_LightPosition, 5.0, 8.0, 7.0); // Set the light direction (in the world coordinate)
+    this.gl.uniform3f(this.u_AmbientLight, 0.2, 0.2, 0.2); // Set the ambient light
+    return;
+  }
+
+  initVertexBuffersForSphere() {
+    if (!this.initArrayBuffer('a_Position', new Float32Array(this.sphere.positions), this.gl.FLOAT, 3)) return false;
+    if (!this.initArrayBuffer('a_Normal', new Float32Array(this.sphere.positions), this.gl.FLOAT, 3))  return false;
+
+    let indexBuffer = this.gl.createBuffer();
+    if (!indexBuffer) {
+      console.error('Failed to create the buffer object');
+      return false;
+    }
+    this.gl.bindBuffer(this.gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+    this.gl.bufferData(this.gl.ELEMENT_ARRAY_BUFFER, new Uint32Array(this.sphere.indices), this.gl.STATIC_DRAW);
+
+    return true;
+  }
+
+  initArrayBuffer(attribute, data, type, num) {
+    let buffer = this.gl.createBuffer();
+    if (!buffer) {
+      console.error('Failed to create the buffer object');
+      return false;
+    }
+
+    this.gl.bindBuffer(this.gl.ARRAY_BUFFER, buffer);
+    this.gl.bufferData(this.gl.ARRAY_BUFFER, data, this.gl.STATIC_DRAW);
+
+    let a_attribute = this.gl.getAttribLocation(this.gl.program, attribute);
+    if (a_attribute < 0) {
+      console.error('Failed to get the storage location of ' + attribute);
+      return false;
+    }
+    this.gl.vertexAttribPointer(a_attribute, num, type, false, 0, 0);
+    this.gl.enableVertexAttribArray(a_attribute);
+
+    this.gl.bindBuffer(this.gl.ARRAY_BUFFER, null);
+
+    return true;
+  }
+
+  draw() {
+    let modelMatrix = new Matrix4();
+    let mvpMatrix = new Matrix4();
+    let normalMatrix = new Matrix4();
+
+    modelMatrix.setRotate(this.sphere.angleZ, 0, 0, 1); // Rotate around the z-axis
+
+    mvpMatrix.setPerspective(30, this.canvas.width / this.canvas.height, 1, 100);
+    mvpMatrix.lookAt(
+      this.camera.pos.x, this.camera.pos.y, this.camera.pos.z, 
+      this.camera.lookAt.x, this.camera.lookAt.y, this.camera.lookAt.z, 
+      this.camera.up.x, this.camera.up.y, this.camera.up.z);
+    mvpMatrix.multiply(modelMatrix);
+
+    normalMatrix.setInverseOf(modelMatrix);
+    normalMatrix.transpose();
+
+    this.gl.uniformMatrix4fv(this.u_ModelMatrix, false, modelMatrix.elements);
+    this.gl.uniformMatrix4fv(this.u_MvpMatrix, false, mvpMatrix.elements);
+    this.gl.uniformMatrix4fv(this.u_NormalMatrix, false, normalMatrix.elements);
+
+    this.gl.clearColor(1, 1, 1, 1)
+    this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
+
+    this.gl.getExtension('OES_element_index_uint'); // for UNSIGNED_INT support
+    //this.gl.drawElements(this.gl.LINE_STRIP, this.vertex_n, this.gl.UNSIGNED_INT, 0);
+    this.gl.drawElements(this.gl.POINTS, this.vertex_n, this.gl.UNSIGNED_INT, 0);
+
+    return;
+  }
+
+  tick() {
+    console.log("drawing...")
+    //updateState();
+    this.draw();
+    requestAnimationFrame(() => {this.tick()});
+
+    return;
+  }
+
+}
+
 
 function main() {
-  // Retrieve <canvas> element
-  var canvas = document.getElementById('webgl');
+  //let sphere = new Sphere(SPIRAL_SPHERE, 100000, 1, {x:0, y:0, z:0});
+  let sphere = new Sphere(CLASSIC_SPHERE, 500, 1, {x:0, y:0, z:0});
 
-  // Get the rendering context for WebGL
-  var gl = getWebGLContext(canvas);
-  if (!gl) {
-    console.log('Failed to get the rendering context for WebGL');
-    return;
-  }
-
-  // Initialize shaders
-  if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
-    console.log('Failed to intialize shaders.');
-    return;
-  }
-
-  // 
-  var n = initVertexBuffers(gl);
-  if (n < 0) {
-    console.log('Failed to set the vertex information');
-    return;
-  }
-
-  // Set the clear color and enable the depth test
-  gl.clearColor(0.0, 0.0, 0.0, 1.0);
-  gl.enable(gl.DEPTH_TEST);
-
-  // Get the storage locations of uniform variables
-  var u_ModelMatrix = gl.getUniformLocation(gl.program, 'u_ModelMatrix');
-  var u_MvpMatrix = gl.getUniformLocation(gl.program, 'u_MvpMatrix');
-  var u_NormalMatrix = gl.getUniformLocation(gl.program, 'u_NormalMatrix');
-  var u_LightColor = gl.getUniformLocation(gl.program, 'u_LightColor');
-  var u_LightPosition = gl.getUniformLocation(gl.program, 'u_LightPosition');
-  var u_AmbientLight = gl.getUniformLocation(gl.program, 'u_AmbientLight');
-  if (!u_ModelMatrix || !u_MvpMatrix || !u_NormalMatrix || !u_LightColor || !u_LightPosition　|| !u_AmbientLight) { 
-    console.log('Failed to get the storage location');
-    return;
-  }
-
-  // Set the light color (white)
-  gl.uniform3f(u_LightColor, 0.8, 0.8, 0.8);
-  // Set the light direction (in the world coordinate)
-  gl.uniform3f(u_LightPosition, 5.0, 8.0, 7.0);
-  // Set the ambient light
-  gl.uniform3f(u_AmbientLight, 0.2, 0.2, 0.2);
-
-  var modelMatrix = new Matrix4();  // Model matrix
-  var mvpMatrix = new Matrix4();    // Model view projection matrix
-  var normalMatrix = new Matrix4(); // Transformation matrix for normals
-
-  // Calculate the model matrix
-  //modelMatrix.setRotate(30, 0, 1, 0); // Rotate around the y-axis
-  //modelMatrix.setRotate(30, 1, 0, 0); // Rotate around the y-axis
-  // Calculate the view projection matrix
-  mvpMatrix.setPerspective(30, canvas.width/canvas.height, 1, 100);
-  mvpMatrix.lookAt(3, 3, 2, 0, 0, 0, 0, 0, 1);
-  mvpMatrix.multiply(modelMatrix);
-  // Calculate the matrix to transform the normal based on the model matrix
-  normalMatrix.setInverseOf(modelMatrix);
-  normalMatrix.transpose();
-
-  // Pass the model matrix to u_ModelMatrix
-  gl.uniformMatrix4fv(u_ModelMatrix, false, modelMatrix.elements);
-
-  // Pass the model view projection matrix to u_mvpMatrix
-  gl.uniformMatrix4fv(u_MvpMatrix, false, mvpMatrix.elements);
-
-  // Pass the transformation matrix for normals to u_NormalMatrix
-  gl.uniformMatrix4fv(u_NormalMatrix, false, normalMatrix.elements);
-
-  // Clear color and depth buffer
-  gl.clearColor(1, 1, 1, 1)
-  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-  // Draw the cube
-  //gl.drawElements(gl.LINE_STRIP, n, gl.UNSIGNED_SHORT, 0);
-  gl.drawElements(gl.POINTS, n, gl.UNSIGNED_SHORT, 0);
-}
-
-function generateClassicSphere() {
-  const SPHERE_DIV = 50;
-  const R = 1;
-
-  let positions = new Array(3 * (SPHERE_DIV + 1) * (SPHERE_DIV + 1));
-  let indices = new Array(4 * SPHERE_DIV * SPHERE_DIV);
-  let iterator = 0;
-
-  // Generate coordinates
-  for (let j = 0; j <= SPHERE_DIV; ++j) {
-    let phi = j * Math.PI / SPHERE_DIV;
-    let sj = Math.sin(phi);
-    let cj = Math.cos(phi);
-
-    for (let i = 0; i <= SPHERE_DIV; ++i) {
-      let theta = 2 * i * Math.PI / SPHERE_DIV;
-      let si = Math.sin(theta);
-      let ci = Math.cos(theta);
-      
-      positions[iterator++] = (R * ci * sj);  // X
-      positions[iterator++] = (R * si * sj);  // Y
-      positions[iterator++] = (R * cj);       // Z
-    }
-  }
-
-  iterator = 0;
-
-  // Generate indices (for *_STRIP methods)
-  for (let j = 0; j < SPHERE_DIV; ++j) {
-    for (let i = 0; i < SPHERE_DIV; ++i) {
-      let p1 = j * (SPHERE_DIV + 1) + i;
-      let p2 = p1 + (SPHERE_DIV + 1);
-
-      indices[iterator++] = p1;
-      indices[iterator++] = p1 + 1;
-      indices[iterator++] = p2;
-      indices[iterator++] = p2 + 1;
-    }
-  }
-
-  return [positions, indices]
-}
-
-function generateSpiralSphere() {
-  const N = 50000;
-  const R = 1;
-
-  let positions = new Array(3 * N);
-  let iterator = 0;
-
-  // Generate coordinates
-  let theta = 0;
-
-  for (let k = 1; k <= N; ++k) {
-    let h = -1 + (2 * k - 2) / (N - 1);
-    let phi = Math.acos(h);
-
-    let s_phi = Math.sin(phi);
-    let c_phi = Math.cos(phi);
-
-    theta = (theta + (3.8 / Math.sqrt(N * (1 - h * h))))// % (2 * Math.PI);
-    if (k == N || k == 1)
-      theta = 0;
-
-    let s_theta = Math.sin(theta);
-    let c_theta = Math.cos(theta);
-      
-    positions[iterator++] = (R * s_phi * c_theta);  // X
-    positions[iterator++] = (R * s_phi * s_theta);  // Y
-    positions[iterator++] = (R * c_phi);       // Z
-  }
-
-  // Generate indices (for POINTS method)
-  let indices = new Array(N);
-  for (let i = 0; i < N; i++)
-    indices[i] = i;
-
-  console.log(positions);
-
-  return [positions, indices]
-}
-
-function initVertexBuffers(gl) { // Create a sphere
-  //data = generateClassicSphere();
-  data = generateSpiralSphere();
-  positions = data[0];
-  indices = data[1];
-
-  // Write the vertex property to buffers (coordinates and normals)
-  // Same data can be used for vertex and normal
-  // In order to make it intelligible, another buffer is prepared separately
-  if (!initArrayBuffer(gl, 'a_Position', new Float32Array(positions), gl.FLOAT, 3)) return -1;
-  if (!initArrayBuffer(gl, 'a_Normal', new Float32Array(positions), gl.FLOAT, 3))  return -1;
-  
-  // Unbind the buffer object
-  gl.bindBuffer(gl.ARRAY_BUFFER, null);
-
-  // Write the indices to the buffer object
-  var indexBuffer = gl.createBuffer();
-  if (!indexBuffer) {
-    console.log('Failed to create the buffer object');
-    return -1;
-  }
-  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
-
-  return indices.length;
-}
-
-function initArrayBuffer(gl, attribute, data, type, num) {
-  // Create a buffer object
-  var buffer = gl.createBuffer();
-  if (!buffer) {
-    console.log('Failed to create the buffer object');
-    return false;
-  }
-  // Write date into the buffer object
-  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-  gl.bufferData(gl.ARRAY_BUFFER, data, gl.STATIC_DRAW);
-  // Assign the buffer object to the attribute variable
-  var a_attribute = gl.getAttribLocation(gl.program, attribute);
-  if (a_attribute < 0) {
-    console.log('Failed to get the storage location of ' + attribute);
-    return false;
-  }
-  gl.vertexAttribPointer(a_attribute, num, type, false, 0, 0);
-  // Enable the assignment of the buffer object to the attribute variable
-  gl.enableVertexAttribArray(a_attribute);
-
-  return true;
+  let graphic = new Graphic(sphere);
+  graphic.draw();
 }
